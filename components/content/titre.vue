b1a80a87bd92b8b619ca20e66c84ab9589878427
Thomas Ruffier <t.ruffier@lapierrequimousse.com>
2024-06-14 11:20:12 +0200

    V2

---
+++ components/content/titre.vue
@@ -0,0 +1,32 @@
+<template>
+  <component :is="as" :class="classComputed">
+    <txt><slot /></txt>
+  </component>
+</template>
+
+<script setup>
+const props = defineProps({
+  as: { default: "div", type: String },
+  class: { default: "", type: String },
+  type: { default: "1", type: String },
+});
+const classComputed = computed(() => {
+  let temp = [];
+  if (props.type == "1") {
+    temp =
+      "font-Titre sm:text-[3.8125rem] text-white font-bold sm:leading-[4rem] text-[2.5rem] leading-10";
+  }
+  if (props.type == "2") {
+    temp =
+      "font-Titre sm:text-[2.5rem] text-[1.8rem] text-white font-bold leading-10";
+  }
+  if (props.type == "3") {
+    temp = "font-Titre text-[2.5rem] text-white font-bold text-center";
+  }
+  if (props.type == "4") {
+    temp =
+      "text-[1.2rem] text-white  text-center font-Titre font-light uppercase";
+  }
+  return [temp, props.class];
+});
+</script>
